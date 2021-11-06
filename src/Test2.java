3dfa8f82a58d139fb1fd39f38b14a86d9bbd4782
akhil <akhil@AKHILESH>
2021-11-06 15:59:27 +0530

    committing Test2 to branch sgi

---
+++ src/Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		String str= "hungey";
+		if(str=="hungry") {
+			System.out.println("eat something....");
+		}else {
+			System.out.println("do some work....");
+		}
+	}
+
+}
