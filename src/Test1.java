6702d8f6b6874e76da45f30104b5e54ba9e6af6a
akhil <akhil@AKHILESH>
2021-11-06 13:11:51 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str= "hungey";
+		if(str=="hungry") {
+			System.out.println("eat something....");
+		}else {
+			System.out.println("do some work....");
+		}
+	}
+
+}
